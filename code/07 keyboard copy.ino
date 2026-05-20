0f2f0202441dde52495f4cd53201faf2df0c61c8
suikanashi <hashikono@outlook.com>
2026-05-20 13:52:46 -0700

    Finished the keyboard stuff plus a display

--- code/07 keyboard copy.ino
+++ code/07 keyboard copy.ino
@@ -1,34 +1,33 @@
-/*
-- left: A0,1,2
-- right: A3,4,5
-- left led: 10
-- right led: 11
-- piezo: 3
-*/
+#include <Adafruit_LiquidCrystal.h>
+Adafruit_LiquidCrystal lcd_1(0);
 
-//sensors
-int 0_value;
-int 1_value;
-int 2_value;
-int 3_value;
-int 4_value;
-int 5_value;
-
-//values
-int s_low = 1023;
-int s_high = 0;
+int notes[] = {262,294,330,349};
 
 void setup() {
-	pinMode(10, OUTPUT);
-    pinMode(11, OUTPUT);
-    pinMode(3, OUTPUT);
-    while(millis() < 5000){
-        0_value = analogRead(A0);
-        1_value = analogRead(A1);
-        2_value = analogRead(A2);
-        3_value = analogRead(A3);
-        4_value = analogRead(A4);
-        5_value = analogRead(A5);
-    }
+    Serial.begin(9600);
+    lcd_1.begin(16, 2);
+    lcd_1.print("notes ");
+    lcd_1.setBacklight(1);
 }
 
+void loop() {
+    int keyVal = analogRead(A0);
+    Serial.println(keyVal);
+    lcd_1.setCursor(6, 0);
+
+    if (keyVal == 1023){
+        tone(8, notes[0]);
+        lcd_1.print(notes[0]);
+    } else if (keyVal >= 990 && keyVal <= 1010){
+        tone(8, notes[1]);
+        lcd_1.print(notes[1]);
+    } else if (keyVal >= 505 && keyVal <= 515){
+        tone(8, notes[2]);
+        lcd_1.print(notes[2]);
+    } else if (keyVal >= 5 && keyVal <= 10){
+        tone(8, notes[3]);
+        lcd_1.print(notes[3]);
+    } else{
+        noTone(8);
+    }
+}
