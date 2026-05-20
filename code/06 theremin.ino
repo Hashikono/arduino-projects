9a665f518d3fd45a2113cb7e501d5c800e760548
suikanashi <hashikono@outlook.com>
2026-05-20 13:37:17 -0700

    Added display and finished stuff

--- code/06 theremin.ino
+++ code/06 theremin.ino
@@ -1,95 +1,42 @@
-/*
-- left: A0,A1,A2
-- right: A3,A4,A5
-- left led: 10
-- right led: 11
-- piezo: 3
-*/
+#include <Adafruit_LiquidCrystal.h>
+Adafruit_LiquidCrystal lcd_1(0);
 
-//sensors
-int x0_value = 0;
-int x1_value = 0;
-int x2_value = 0;
-int x3_value = 0;
-int x4_value = 0;
-int x5_value = 0;
-
+//regular stuff
+int sensorValue;
 int sensorLow = 1023;
 int sensorHigh = 0;
-
-float left;
-float right;
-
-//values
-int s_low = 1023;
-int s_high = 0;
-
-float sValue(){
-    x0_value = analogRead(A0);
-    x1_value = analogRead(A1);
-    x2_value = analogRead(A2);
-    x3_value = analogRead(A3);
-    x4_value = analogRead(A4);
-    x5_value = analogRead(A5);
-
-    left = (x0_value + x1_value + x2_value)/3;
-    right = (x3_value + x4_value + x5_value)/3;
-    return (left+right)/2;
-}
+const int ledPin = 13;
 
 void setup() {
-  	Serial.begin(9600);
-	pinMode(10, OUTPUT);
-    pinMode(11, OUTPUT);
-    pinMode(3, OUTPUT);
-    while(millis() < 500){
-        x0_value = analogRead(A0);
-        x1_value = analogRead(A1);
-        x2_value = analogRead(A2);
-        x3_value = analogRead(A3);
-        x4_value = analogRead(A4);
-        x5_value = analogRead(A5);
+    pinMode(ledPin, OUTPUT);
+    digitalWrite(ledPin, HIGH);
+    
+    while (millis() < 1000) {
+        sensorValue = analogRead(A0);
+        
+        if (sensorValue > sensorHigh) {
+            sensorHigh = sensorValue;
+        }
+        
+        if (sensorValue < sensorLow) {
+            sensorLow = sensorValue;
+        }
     }
-
-    //combination
-    left = (x0_value + x1_value + x2_value)/3;
-    right = (x3_value + x4_value + x5_value)/3;
-    float sensorValue = (left+right)/2;
-
-    //don't know
-    if (sensorValue > sensorHigh) {
-        sensorHigh = sensorValue;
-    }
-    if (sensorValue < sensorLow) {
-        sensorLow = sensorValue;
-    }
-
-    analogWrite(10, left/500);
-    analogWrite(11, right/500);
-  	Serial.println(analogRead(A0));
-    Serial.println(analogRead(A1));
-    Serial.println(analogRead(A2));
-    Serial.println(analogRead(A3));
-    Serial.println(analogRead(A4));
-    Serial.println(analogRead(A5));
-    Serial.println(right);
-    Serial.print(left);
+    
+    digitalWrite(ledPin, LOW);
+    lcd_1.begin(16, 2);
+    lcd_1.print("value ");
+    lcd_1.setBacklight(1);
 }
 
 void loop() {
-    float sens = sValue();
-    int pitch = map(sens,sensorLow,sensorHigh, 50, 4000);
-    tone(3, pitch,20);
-    analogWrite(10, left/500);
-    analogWrite(11, right/500);
+    sensorValue = analogRead(A0);
+    int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 4000);
+    tone(8, pitch, 20);
+    
+    //display
+    lcd_1.setCursor(6, 0);
+    lcd_1.print(sensorValue);
     delay(10);
-    Serial.println(analogRead(A0));
-    Serial.println(analogRead(A1));
-    Serial.println(analogRead(A2));
-    Serial.println(analogRead(A3));
-    Serial.println(analogRead(A4));
-    Serial.println(analogRead(A5));
-    Serial.println(right);
-    Serial.print(left);
 }
 
